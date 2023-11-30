b4cd3da3243e03cc2276fb574f486093d5c0c0d7
Tasdir Ahmmed <2222325@iub.edu.bd>
2023-12-01 01:01:41 +0600

    initial admin pie chart

--- admin/index.php
+++ admin/index.php
@@ -2,76 +2,54 @@
 if($_SESSION['ADMIN_ROLE'] == '0'){
     redirect('product');
 }
+$total_users = mysqli_num_rows(mysqli_query($con,"select * from users where email_verification='1'"));
+$total_farmers = mysqli_num_rows(mysqli_query($con,"select * from admin where email_verification='1' and roll='0'"));
+$total_delivery_boy = mysqli_num_rows(mysqli_query($con,"select * from delivery_boy where status='1'"));
 ?>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels"></script> <!-- Datalabels plugin -->
 
 <div class="row">
-    <?php 
-        $total_users = mysqli_num_rows(mysqli_query($con,"select * from users where email_verification='1'"));
-        $total_farmers = mysqli_num_rows(mysqli_query($con,"select * from admin where email_verification='1' and roll='0'"));
-        $total_delivery_boy = mysqli_num_rows(mysqli_query($con,"select * from delivery_boy where status='1'"));
-    ?>
-
     <!-- Chart 1 -->
     <div class="col-md-4">
         <canvas id="chartUsers"></canvas>
     </div>
-
-    <!-- Chart 2 -->
-    <div class="col-md-4">
-        <canvas id="chartFarmers"></canvas>
-    </div>
-
-    <!-- Chart 3 -->
-    <div class="col-md-4">
-        <canvas id="chartDeliveryBoy"></canvas>
-    </div>
-
-    <script>
-        var totalUsers = <?php echo $total_users; ?>;
-        var totalFarmers = <?php echo $total_farmers; ?>;
-        var totalDeliveryBoy = <?php echo $total_delivery_boy; ?>;
-
-        // Chart for Users
-        new Chart("chartUsers", {
-            type: "pie",
-            data: {
-                labels: ["Customers"],
-                datasets: [{
-                    backgroundColor: ["#b91d47"],
-                    data: [totalUsers]
-                }]
-            },
-            options: { title: { display: true, text: "Customers" }}
-        });
-
-        // Chart for Farmers
-        new Chart("chartFarmers", {
-            type: "pie",
-            data: {
-                labels: ["Farmers"],
-                datasets: [{
-                    backgroundColor: ["#00aba9"],
-                    data: [totalFarmers]
-                }]
-            },
-            options: { title: { display: true, text: "Farmers" }}
-        });
-
-        // Chart for Delivery Boys
-        new Chart("chartDeliveryBoy", {
-            type: "pie",
-            data: {
-                labels: ["Delivery Boys"],
-                datasets: [{
-                    backgroundColor: ["#ff6347"],
-                    data: [totalDeliveryBoy]
-                }]
-            },
-            options: { title: { display: true, text: "Delivery Boys" }}
-        });
-    </script>
 </div>
+
+<script>
+    var totalUsers = <?php echo $total_users; ?>;
+    var totalFarmers = <?php echo $total_farmers; ?>;
+    var totalDeliveryBoy = <?php echo $total_delivery_boy; ?>;
+
+    var ctx = document.getElementById('chartUsers').getContext('2d');
+    new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ["Customers", "Farmers", "Delivery Men"],
+            datasets: [{
+                backgroundColor: ["#b91d47", "#00aba9", "#ff6347"],
+                data: [totalUsers, totalFarmers, totalDeliveryBoy]
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'User Distribution'
+            },
+            plugins: {
+                datalabels: {
+                    color: '#fff',
+                    formatter: (value, ctx) => {
+                        return value;
+                    },
+                    font: {
+                        weight: 'bold'
+                    }
+                }
+            }
+        }
+    });
+</script>
 
 <?php include('footer.php'); ?>
