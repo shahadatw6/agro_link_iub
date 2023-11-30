dbd510f950e35318cd9566a78dfd15a16d5108e1
Tasdir Ahmmed <2222325@iub.edu.bd>
2023-12-01 01:46:47 +0600

    add bar chart on admin panel

--- admin/index.php
+++ admin/index.php
@@ -11,19 +11,25 @@ $total_delivery_boy = mysqli_num_rows(mysqli_query($con,"select * from delivery_
 <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels"></script> <!-- Datalabels plugin -->
 
 <div class="row">
-    <!-- Chart 1 -->
-    <div class="col-md-4">
+    <!-- Pie Chart -->
+    <div class="col-md-6">
         <canvas id="chartUsers"></canvas>
+    </div>
+
+    <!-- Bar Chart -->
+    <div class="col-md-6">
+        <canvas id="barChart"></canvas>
     </div>
 </div>
 
 <script>
+    // Existing script for pie chart
     var totalUsers = <?php echo $total_users; ?>;
     var totalFarmers = <?php echo $total_farmers; ?>;
     var totalDeliveryBoy = <?php echo $total_delivery_boy; ?>;
 
-    var ctx = document.getElementById('chartUsers').getContext('2d');
-    new Chart(ctx, {
+    var ctxPie = document.getElementById('chartUsers').getContext('2d');
+    new Chart(ctxPie, {
         type: 'pie',
         data: {
             labels: ["Customers", "Farmers", "Delivery Men"],
@@ -38,6 +44,37 @@ $total_delivery_boy = mysqli_num_rows(mysqli_query($con,"select * from delivery_
                 text: 'User Distribution'
             },
 
+        }
+    });
+
+    // Data for bar chart
+    // Replace with your actual data
+    var barChartData = {
+        labels: ['Oct', 'Nov', 'Dec'], // Replace with your labels
+        datasets: [{
+            label: 'Order Chart',
+            backgroundColor: '#3498db',
+            borderColor: '#2980b9',
+            data: [20, 30, 40] // Replace with your data
+        }]
+    };
+
+    var ctxBar = document.getElementById('barChart').getContext('2d');
+    new Chart(ctxBar, {
+        type: 'bar',
+        data: barChartData,
+        options: {
+            title: {
+                display: true,
+                text: 'Order Chart'
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
         }
     });
 </script>
