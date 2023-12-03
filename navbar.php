73fa5a24bd7bc43ac23cbb3080f5f817fa346e14
Md Shahadat Hossain Shahal <shahadatw6@gmail.com>
2023-12-03 16:27:46 +0600

    correcting paths

---
+++ navbar.php
@@ -0,0 +1,29 @@
+<!-- Start Navigation -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-default bootsnav">
+    <div class="container">
+        <!-- Start Header Navigation -->
+        <div class="navbar-header">
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbar-menu"
+                aria-controls="navbars-rs-food"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand" href="index.html">
+                <img
+                    src="images/new data/logo.png"
+                    class="logo"
+                    alt=""
+                    width="100"
+                    height="85"
+                />
+            </a>
+        </div>
+        <!-- End Header Navigation -->
+    </div>
+</nav>
