c71d3cfda4a06f25ce5a4a700424b41fcc8dc706
Faiyaz Zaman <faiyaz.zaman10@gmail.com>
2023-12-08 00:27:59 +0600

    add delivery boy using a form

--- admin/manage_delivery_boy.php
+++ admin/manage_delivery_boy.php
@@ -1,13 +1,14 @@
 <?php
 include('top.php');
 if ($_SESSION['ADMIN_ROLE'] == '0') {
-	redirect('product');
+	redirect('employee');
 }
 $msg = "";
 $name = "";
 $mobile = "";
 $password = "";
 $id = "";
+$email = "";
 
 if (isset($_GET['id']) && $_GET['id'] > 0) {
 	$id = get_safe_value($_GET['id']);
@@ -15,30 +16,32 @@ if (isset($_GET['id']) && $_GET['id'] > 0) {
 	$name = $row['emp_name'];
 	$password = $row['password'];
 	$mobile = $row['emp_contact'];
+	$email = $row['email'];
 }
 
 if (isset($_POST['submit'])) {
 	$name = get_safe_value($_POST['name']);
 	$password = get_safe_value($_POST['password']);
 	$mobile = get_safe_value($_POST['mobile']);
-	$added_on = date('Y-m-d h:i:s');
+	$email = get_safe_value($_POST['email']);
+
 
 	if ($id == '') {
-		$sql = "select * from delivery_boy where mobile='$mobile'";
+		$sql = "select * from employee where emp_contact='$mobile'";
 	} else {
-		$sql = "select * from delivery_boy where mobile='$mobile' and id!='$id'";
+		$sql = "select * from employee where emp_contact='$mobile' and employee_ID!='$id'";
 	}
 	if (mysqli_num_rows(mysqli_query($con, $sql)) > 0) {
 		$msg = "Delivery boy already added";
 	} else {
 		if ($id == '') {
 
-			mysqli_query($con, "insert into delivery_boy(name,password,mobile,status,added_on) values('$name','$password','$mobile',1,'$added_on')");
+			mysqli_query($con, "insert into employee(emp_name,emp_contact,department,usertype,email,username,password,status) values('$name','$mobile','delivery Boy',5,'$email','$name','$password',1)");
 		} else {
-			mysqli_query($con, "update delivery_boy set name='$name', password='$password' , mobile='$mobile' where id='$id'");
+			mysqli_query($con, "update employee set emp_name='$name', password='$password' , emp_contact='$mobile' where employee_ID='$id'");
 		}
 
-		redirect('delivery_boy');
+		redirect('employee');
 	}
 }
 ?>
@@ -61,8 +64,12 @@ if (isset($_POST['submit'])) {
 						<label for="exampleInputEmail3" required>Password</label>
 						<input type="textbox" class="form-control" placeholder="Password" name="password" value="<?php echo $password ?>">
 					</div>
+					<div class="form-group">
+						<label for="exampleInputName1">Email</label>
+						<input type="text" class="form-control" placeholder="Email" name="email" required value="<?php echo $email ?>">
+						<div class="error mt8"><?php echo $msg ?></div>
 
-					<button type="submit" class="btn btn-primary mr-2" name="submit">Submit</button>
+						<button type="submit" class="btn btn-primary mr-2" name="submit">Submit</button>
 				</form>
 			</div>
 		</div>
