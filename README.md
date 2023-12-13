0ad5d2289c0daa7696948a21fa759d0e824062d3
Md Shahadat Hossain Shahal <shahadatw6@gmail.com>
2023-12-14 01:11:34 +0600

    Remove files from master branch

--- README.md
+++
@@ -1,1 +0,0 @@
-to get the lit review: [Literature_Review.pdf](Literature_Review.pdf)
--- admin/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-include('../includes/function.inc.php');
-unset( $_SESSION['ADMIN_LOGIN']);
-redirect('admin_login');
-?>
