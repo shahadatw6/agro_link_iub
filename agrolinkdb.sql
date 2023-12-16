a16b46bf6a0b437fdce9dd7f88928f139d4b9a94
Md Shahadat Hossain Shahal <shahadatw6@gmail.com>
2023-12-17 04:02:53 +0600

    couse material

--- agrolinkdb.sql
+++ agrolinkdb.sql
@@ -15,8 +15,7 @@ CREATE TABLE EMPLOYEE (
     password VARCHAR(255) NOT NULL,
     CONSTRAINT empPK PRIMARY KEY (employee_ID)
 );
-ALTER TABLE EMPLOYEE
-ADD COLUMN status TINYINT DEFAULT 1 NOT NULL;
+
 
 INSERT INTO EMPLOYEE(emp_name, emp_contact, department,usertype, email, username,password) VALUES
 ('Shahal', '01877585773', 'admin', '0','shahadatw6@gmail.com','shahal', '123'),
